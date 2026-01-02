e1e445b178e0e93784130c931e55f4da9d758740
hummodi6991 <ahmed.khayat.96@gmail.com>
2026-01-02 16:18:12 +0300

    Adjust contingency default to five percent

--- tests/excel_inputs.py
+++ tests/excel_inputs.py
@@ -9,7 +9,7 @@ def sample_excel_inputs() -> dict:
         "cp_sqm_per_space": {"basement": 30},
         "rent_sar_m2_yr": {"residential": 2400},
         "fitout_rate": 400,
-        "contingency_pct": 0.10,
+        "contingency_pct": 0.05,
         "consultants_pct": 0.06,
         "transaction_pct": 0.03,
         "land_price_sar_m2": 2800,
