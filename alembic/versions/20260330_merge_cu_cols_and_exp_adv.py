b5a9c2a8f82120cb7f38b2f9e6dd5f61e65ca2bb
hummodi6991 <ahmed.khayat.96@gmail.com>
2026-03-31 16:46:46 +0000

    fix: correct revision ID in merge migration (was duplicate of child)

--- alembic/versions/20260330_merge_cu_cols_and_exp_adv.py
+++ alembic/versions/20260330_merge_cu_cols_and_exp_adv.py
@@ -5,7 +5,7 @@ Revises: 20260330_widen_cu_cols, 20260330_exp_adv_commercial_units
 Create Date: 2026-03-30
 """
 
-revision = "20260330_merge_cu_exp_adv"
+revision = "20260330_merge_cu_cols_and_exp_adv"
 down_revision = ("20260330_widen_cu_cols", "20260330_exp_adv_commercial_units")
 branch_labels = None
 depends_on = None
