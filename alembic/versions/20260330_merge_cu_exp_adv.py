26642c7af5a5df038c10b0e039f548a069b76e3b
hummodi6991 <ahmed.khayat.96@gmail.com>
2026-03-31 16:52:25 +0000

    fix: correct second parent revision ID in merge migration

--- alembic/versions/20260330_merge_cu_exp_adv.py
+++ alembic/versions/20260330_merge_cu_exp_adv.py
@@ -1,14 +1,14 @@
 """merge cu/exp_adv and geography heads
 
 Revision ID: 20260330_merge_cu_exp_adv
-Revises: 20260330_merge_cu_cols_and_exp_adv, 20260322_ea_geography_gist_indexes
+Revises: 20260330_merge_cu_cols_and_exp_adv, 20260322_ea_geog_gist
 Create Date: 2026-03-30
 """
 
 from alembic import op
 
 revision = "20260330_merge_cu_exp_adv"
-down_revision = ("20260330_merge_cu_cols_and_exp_adv", "20260322_ea_geography_gist_indexes")
+down_revision = ("20260330_merge_cu_cols_and_exp_adv", "20260322_ea_geog_gist")
 branch_labels = None
 depends_on = None
 
